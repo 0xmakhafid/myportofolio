f18fb1a4766d7649af248b43641f4eaa7aae8372
0xMakhafid <158286092+0xmakhafid@users.noreply.github.com>
2025-03-17 08:34:33 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,13 @@ const projects = [
     image: "/masprofile_450x800.jpg",
     link: "https://open.substack.com/pub/makhafid/p/membangun-website-profil-sekolah?r=4qh0pz&utm_campaign=post&utm_medium=web&showWelcomeOnShare=false",
   },  
+      {
+    id: 7,
+    title: "Optimasi Performa Website: Raih Skor 100/100/96 Berkat Perbaikan Teknis",
+    description: "Hasil ini membuktikan kemampuan saya dalam meningkatkan kualitas dan standar web secara menyeluruh.",
+    image: "/optimasi-web.jpg",
+    link: "/optimasi_masahmadsaidkudus.html",
+  },
   {
     id: 2,
     title: "Sistem PPDB Online - MA Salafiyah Ahmad Said Kudus",
@@ -51,13 +58,6 @@ const projects = [
   image: "/portfolio.jpg",
   link: "https://github.com/0xmakhafid/myportofolio"
 },
-    {
-    id: 7,
-    title: "Optimasi Performa Website: Raih Skor 100/100/96 Berkat Perbaikan Teknis,
-    description: "Hasil ini membuktikan kemampuan saya dalam meningkatkan kualitas dan standar web secara menyeluruh.",
-    image: "/optimasi-web.jpg",
-    link: "https://heikhafid.my.id/optimasi_masahmadsaidkudus.html",
-  },
 ]
 
 const certificates = [
